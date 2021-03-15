70b6b9e929ea500d206b66fbafb1c1a9cdfc804c
Cr4ckC4t <63863112+Cr4ckC4t@users.noreply.github.com>
2021-03-15 19:48:00 +0100

    Upload windows keylogger source code

---
+++ colors.h
@@ -0,0 +1,17 @@
+#ifndef GUARD_COLORS
+#define GUARD_COLORS
+
+/*
+*   Formatted color code strings.
+*/
+
+#define FC_END      "\033[0m"
+#define FC_BLACK    "\033[0;30m"
+#define FC_RED      "\033[0;31m"
+#define FC_GREEN    "\033[0;32m"
+#define FC_YELLOW   "\033[0;33m"
+#define FC_BLUE     "\033[0;34m"
+#define FC_PURPLE   "\033[0;35m"
+#define FC_CYAN     "\033[0;36m"
+
+#endif
